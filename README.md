7e0d68a5f9d33aff484697d60d91d64740042e96
LeoTindall <lfstindall@gmail.com>
2017-11-03 20:47:13 -0500

    Basic version of addon

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# randomua
-A browser extension that changes your User-Agent header to throw off tracking.
+# RandomUA
+
+RandomUA is a WebExtension that changes your User-Agent string to a sensible-looking, desktop-browser-like UA string which is different for every request.
+
+Many browser fingerprinting solutions use the User-Agent as a relatively constant string, because most users UAs only change when their browser updates. Therefore, having a different UA on each request make it a lot easier to evade these tracking schemes.
+
+## Permissions
+
+RandomUA requires access to all URLs and web request data for its core function: it has to be able to change your user agent string!
---
+++ addon/background/random-ua.js
@@ -0,0 +1,57 @@
+function choose(choices) {
+    var index = Math.floor(Math.random() * choices.length);
+    return choices[index];
+}
+
+let operating_systems = [
+    "Windows NT", 
+    "X11; Linux x86_64", 
+    "Linux i686", 
+    "Linux i686 on x86_64", 
+    "Intel Mac OS X", 
+    "PPC Mac OS X"
+];
+// Get an operating system name and version.
+function get_os() {
+    let x = Math.floor(Math.random() * 10);
+    let y = Math.floor(Math.random() * 100);
+    let choice = choose(operating_systems);
+    // Linux doesn't use version numbers.
+    if (!choice.includes("Linux")) {
+        choice += " " + x + "." + y
+    }
+    return choice;
+}
+
+// List of simple browsers to apply
+let browsers = ["Firefox", "Chrome", "Safari", "Trident", "OPR", "AppleWebKit"];
+
+// Either add a KHTML clause, or don't.
+let khtml = [" (KHTML, like Gecko) ", " Gecko/20100101 "];
+
+browser.webRequest.onBeforeSendHeaders.addListener(function (request) {
+    // Abort if the request is nonexistant or doesn't use headers.
+    if (!request || !request.requestHeaders) {
+      return {};
+    }
+
+    // Find the header called "User-Agent" or "user-agent" or "UsEr-AgEnT" or whatever, and modift it
+    for (let header of request.requestHeaders) {
+        if (header.name.toLowerCase() === "user-agent") {
+            // Compute a random OS string.
+            let os = get_os();
+            // Compute a random Firefox version, from 0 to 100.
+            let fv = "" + Math.floor(Math.random() * 1000) + ".0";
+            // Choose from the list of browsers.
+            let br = choose(browsers);
+            // Choose additional clause, if any
+            let kh = choose(khtml);
+            // Generate a possible second browser clause
+            let bc = " " + choose(browsers) + "/" + Math.floor(Math.random() * 1000) + ".0";
+            bc = choose([bc, ""]);
+            header.value = "Mozilla/5.0 (" + os + "; rv:" + fv + ")" + kh + bc + " " + br + "/" + fv + ";"
+            console.log("[RANDOMUA] Fake User-Agent set: ", header.value);
+        }
+    }
+    return {requestHeaders: request.requestHeaders};
+  }, {urls: ['<all_urls>']}, ['blocking' ,'requestHeaders']);
