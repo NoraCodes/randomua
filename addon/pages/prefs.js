9b2ed1fe4230c7b79cf279ffecefe1d63a274167
LeoTindall <lfstindall@gmail.com>
2017-11-08 15:23:53 -0600

    Add minimum and maximum version settings.

--- addon/pages/prefs.js
+++ addon/pages/prefs.js
@@ -1,7 +1,9 @@
 // Save the contents of the preferences form
 function saveOptions(e) {
     browser.storage.sync.set({
-        exclusions: document.querySelector("#exclusions").value
+        exclusions: document.querySelector("#exclusions").value,
+        minVersion: document.querySelector("#minVersion").value,
+        maxVersion: document.querySelector("#maxVersion").value,
     });
     browser.storage.local.set({
         useMobile: document.querySelector('#use-mobile').checked
@@ -14,10 +16,12 @@ function saveOptions(e) {
 
 // Show currently selected options
 function restoreOptions() {
-    var gettingItem = browser.storage.sync.get('exclusions');
+    var gettingItem = browser.storage.sync.get();
     gettingItem.then((res) => {
         document.querySelector("#exclusions-text").innerText = res.exclusions || 'no exclusions configured';
         document.querySelector("#exclusions").value = res.exclusions || '';
+        document.querySelector("#minVersion").value = res.minVersion || 0;
+        document.querySelector("#maxVersion").value = res.maxVersion || 250;
     });
 
     var gettingLocalItem = browser.storage.local.get('useMobile');
